bf30f6273726ac84f51a45e3939abf73907e4aa6
caifei <caifei.c@qq.com>
2019-06-19 14:35:16 +0800

    解决android 报错

--- android/src/main/java/com/infitio/adharasocketio/AdharaSocketIoPlugin.java
+++ android/src/main/java/com/infitio/adharasocketio/AdharaSocketIoPlugin.java
@@ -74,7 +74,7 @@ public class AdharaSocketIoPlugin implements MethodCallHandler {
                         if (transports != null) {
                             options.transports = AdharaSocketIoPlugin.getStringArray(transports);
                         }
-                        options.timeout = (Long) call.argument("timeout");
+                        options.timeout = (Integer) call.argument("timeout");
                     }catch (Exception e){
                         Log.e(TAG, e.toString());
                     }
